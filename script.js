9dc551682921f4cfd42753aacdaaa6a51755256d
jax-01 <jrcastillo6@up.edu.ph>
2024-07-30 14:34:43 +0800

    Show computer choice in a p tag

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3);
-    if (choice === 0) return "rock";
-    else if (choice === 1) return "paper";
-    else return "scissors";
+    
+    if (choice === 0) {
+        document.getElementById("computer-choice").innerHTML = "Computer: Rock";
+        return "rock";
+    } else if (choice === 1) { 
+        document.getElementById("computer-choice").innerHTML = "Computer: Paper";
+        return "paper";
+    } else {
+        document.getElementById("computer-choice").innerHTML = "Computer: Scissors";
+        return "scissors";
+    }
 }
+
